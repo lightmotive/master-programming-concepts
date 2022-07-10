b2ea6aa6aa39ab9e4589d6ea5062876ef538755c
Andre Light <andrelight.al@gmail.com>
2022-07-10 23:51:02 +0000

    Simplify function processing slightly

--- recursion/01_basic.rb
+++ recursion/01_basic.rb
@@ -9,10 +9,12 @@ num = 123
 # p num.to_s.chars.map(&:to_i).sum
 
 def sum_digits(num)
-  chars = num.to_s.chars
+  string = num.to_s
+  # That line works whether `num` is an Integer or a String because
+  # **String#to_s** returns `self`.
+  return string.to_i if string.length == 1
 
-  return chars.first.to_i if chars.size == 1
-
+  chars = string.chars
   sum_digits(chars.first) + sum_digits(chars[1..].join)
 end
 
