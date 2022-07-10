9c5d1ade12d890bcaca0b5d4133e5f82addc756f
Andre Light <andrelight.al@gmail.com>
2022-07-10 23:14:56 +0000

    Expand basic problem using iteration

--- recursion/01_basic.rb
+++ recursion/01_basic.rb
@@ -5,4 +5,17 @@
 # - Sum each character converted to a number.
 
 num = 123
-p num.to_s.chars.map(&:to_i).sum
+# p num.to_s.chars.map(&:to_i).sum
+
+def sum_digits(num)
+  chars = num.to_s.chars
+  sum = 0
+
+  chars.each do |char|
+    sum += char.to_i
+  end
+
+  sum
+end
+
+p sum_digits(num)
