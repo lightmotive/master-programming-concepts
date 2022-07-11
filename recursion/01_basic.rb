4e60047b88bc86c1deeb738c4bc18d550b9c2023
Andre Light <andrelight.al@gmail.com>
2022-07-11 00:11:46 +0000

    Initialize second problem: sum of absolute diffs
    - Process the problem: [PE]DAC

--- recursion/01_basic.rb
+++ recursion/01_basic.rb
@@ -19,3 +19,26 @@ def sum_digits(num)
 end
 
 p sum_digits(num)
+
+# Another problem:
+# - Get the absolute sum of the differences between two arrays of integers of
+# the same length.
+
+# Input: Two arrays of integers that are the same length
+# Output: The sum of the absolute difference between the index-correlated
+#         numbers in 2 arrays.
+
+# Rules
+# - Each array contains only integers.
+# - Each array is the same length.
+
+# Examples/Tests
+# sum_absolute_differences([1], [2, 3]) => ArgumentError
+# sum_absolute_differences([3, 7], [19, 12]) == 21
+# sum_absolute_differences(15,-4,56,10,-23], [14,-9,56,14,-23]) == 10
+
+# Data Structure
+
+# Algorithm
+
+# Code
