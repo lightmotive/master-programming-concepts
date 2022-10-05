762fe19e2ac0218ab14d4e32ab4dc6b8f91e2404
Andre Light <andrelight.al@gmail.com>
2022-10-05 22:37:36 +0000

    Expand `UniqueNames` test coverage: other formats

--- encapsulation/robots/test/unique_names_test.rb
+++ encapsulation/robots/test/unique_names_test.rb
@@ -5,36 +5,27 @@ require './lib/unique_names'
 
 class UniqueNamesTest < MiniTest::Test
   DIFFERENT_NAME_SEED = 1234
-  NAME_REGEXP = /\A[A-Z]{2}\d{3}\z/.freeze
+  DEFAULT_NAME_REGEXP = /\A[A-Z]{1}\d{1}\z/.freeze
+  DEFAULT_UNIQUE_NAME_COUNT = 260
 
   def setup
-    @names = UniqueNames.new
-    @default_unique_names_size = 676_000
+    @names = UniqueNames.new(1, 1)
   end
 
   def test_default_name_format
-    assert_match NAME_REGEXP, @names.use!
-  end
-
-  def test_custom_name_formats
-    format1 = /\A[A-Z]{1}\d{1}\z/
-    names1 = UniqueNames.new(1, 1)
-    assert_match(format1, names1.use!)
-    format2 = /\A[A-Z]{3}\d{2}\z/
-    names2 = UniqueNames.new(3, 2)
-    assert_match(format2, names2.use!)
+    assert_match DEFAULT_NAME_REGEXP, @names.use!
   end
 
   def test_all_names_unique
     names = @names.sequences.size.times.map { @names.use! }
-    assert_equal(@default_unique_names_size, names.uniq.size)
+    assert_equal(DEFAULT_UNIQUE_NAME_COUNT, names.uniq.size)
     assert_empty(@names.to_a)
     error = assert_raises(StandardError) { @names.use! }
     assert_equal('No names available', error.message)
   end
 
   def test_sequences_enumerator_size
-    assert_equal(@default_unique_names_size, @names.sequences.size)
+    assert_equal(DEFAULT_UNIQUE_NAME_COUNT, @names.sequences.size)
   end
 
   def test_release_name
@@ -43,7 +34,7 @@ class UniqueNamesTest < MiniTest::Test
     @names.release!(name)
     name2 = @names.use!
     refute_equal name, name2
-    assert_match NAME_REGEXP, name2
+    assert_match DEFAULT_NAME_REGEXP, name2
   end
 
   def test_released_names_are_returned_to_end_of_available_names
@@ -51,5 +42,14 @@ class UniqueNamesTest < MiniTest::Test
     3.times { names << @names.use! }
     names.each { |name| @names.release!(name) }
     assert_equal(names, @names[-3, 3])
+  end
+
+  def test_custom_name_formats
+    names_default = UniqueNames.new
+    assert_match(/\A[A-Z]{2}\d{3}\z/, names_default.use!)
+    assert_equal(676_000, names_default.sequences.size)
+    names2 = UniqueNames.new(3, 2)
+    assert_match(/\A[A-Z]{3}\d{2}\z/, names2.use!)
+    assert_equal(1_757_600, names2.sequences.size)
   end
 end
